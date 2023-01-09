85e3eaf3b36acb681510fad53f7a5016f5747f0b
TFC <1429316040@qq.com>
2023-01-09 23:10:25 +0800

    初步创建安卓应用

--- OpenDoor/app/src/main/java/com/example/opendoor/MainActivity.kt
+++ OpenDoor/app/src/main/java/com/example/opendoor/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import world.shanya.serialport.SerialPortBuilder
+import serialport.SerialPortBuilder
+import android.annotation.SuppressLint
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
