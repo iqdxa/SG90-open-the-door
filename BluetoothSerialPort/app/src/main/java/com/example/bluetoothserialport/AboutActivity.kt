3c105c319043fd8dec0c913780141de96dbb65d0
TFC <1429316040@qq.com>
2023-01-23 23:00:03 +0800

    修改关于界面

--- BluetoothSerialPort/app/src/main/java/com/example/bluetoothserialport/AboutActivity.kt
+++ BluetoothSerialPort/app/src/main/java/com/example/bluetoothserialport/AboutActivity.kt
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import mehdi.sakout.aboutpage.AboutPage
 import mehdi.sakout.aboutpage.Element
 //import world.shanya.serialportsample.utils.CheckUpdate
 import java.lang.Exception
-
 
 class AboutActivity : AppCompatActivity() {
 
@@ -18,35 +18,48 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        val authorElement = Element()
+        authorElement.gravity = Gravity.CENTER
+        authorElement.title = "作者: TFC"
+
         val versionElement = Element()
         versionElement.gravity = Gravity.CENTER
         versionElement.title = "版本号: " + packageManager.getPackageInfo(packageName, 0).versionName
-        versionElement.setOnClickListener {
-            //checkUpdate.check(true)
-        }
 
-        val authorElement = Element()
-        authorElement.gravity = Gravity.CENTER
-        authorElement.title = "作者: Shanya"
+        val upgradeElement = Element()
+        upgradeElement.gravity = Gravity.CENTER
+        upgradeElement.title = "检测更新"
+        upgradeElement.setOnClickListener {
+            //checkUpdate.check(true)
+            AlertDialog.Builder(this).apply{
+                setTitle("检测更新")
+                setMessage("暂无更新")
+                setCancelable(false)
+                setPositiveButton("确定") { dialog, which ->
+                }
+            show()
+            }
+        }
 
         val aboutPage: View = AboutPage(this)
             .isRTL(false)
-            .setDescription("SerialPort封装库的全功能示例APP")
+            .setDescription("蓝牙开门手机端")
 //            .setCustomFont(String) // or Typeface
             .setImage(R.mipmap.ic_launcher_logo)
 //            .addItem(Element().setTitle(packageManager.getPackageInfo(packageName, 0).versionName))
 //            .addItem(adsElement)
             .addGroup("Connect with us")
-            .addEmail("shanyaliux@qq.com")
-            .addWebsite("https://shanyaliux.cn/serialport/")
+//            .addEmail("shanyaliux@qq.com")
+//            .addWebsite("https://shanyaliux.cn/serialport/")
 //            .addFacebook("the.medy")
 //            .addTwitter("medyo80")
 //            .addYoutube("UCdPQtdWIsg7_pi4mrRu46vA")
 //            .addPlayStore("com.ideashower.readitlater.pro")
-            .addGitHub("Shanyaliux\\SerialPortSample")
+            .addGitHub("iqdxa\\SG90-open-the-door")
 //            .addInstagram("medyo80")
             .addItem(authorElement)
             .addItem(versionElement)
+            .addItem(upgradeElement)
             .create()
         setContentView(aboutPage)
     }
