dfc1b98c2b5daf2f69a1f3b31d3b5b73d65bb396
TFC <1429316040@qq.com>
2023-01-26 22:37:25 +0800

    实现接收蓝牙蓝牙信号并做出相应动作

--- 舵机开门/main.c
+++ 舵机开门/main.c
@@ -5,13 +5,15 @@
 #define uint unsigned int
 
 sbit pwm = P1^0;
-sbit sw1 = P3^1;
-sbit sw2 = P3^0;
-
+sbit sw1 = P3^2;
+sbit sw2 = P3^3;
+bit New_rec = 0, Send_ed = 1;
+uint UART_buff;
 //TODO:让每个对应的值相等，即angle是多少就是多少度
 //5对应0度 10对应45度 15对应90度 20对应135度 25对应180度
 uint angle=0;//调整初始角度为0°
 uint count=0;
+int i=0;
 
 void delay(uint ms)
 {
@@ -23,18 +25,29 @@ void delay(uint ms)
 void Init()
 {
 	//Time0_init
-	TMOD = 0x01;//定时器零工作方式1
+	TMOD |= 0x01;//定时器零工作方式1
 	TL0 = 0xA3;
 	TH0 = 0xFF;//初始值500us
 	ET0 = 1;
 	TF0 = 0;//清除TF0标志
 	TR0 = 1;//定时器开始计时
-	EA = 1;//中断总开关
+	
+	//接收中断初始化
+	SCON=0x50;//设定串口工作方式0101 0000
+	PCON=0x00;
+	TMOD |=0x20;
+	TL1=0xfd;//波特率9600
+	TH1=0xfd;
+	TR1=1;
+	
+	
+	EA = 1;
+	ES  = 1;       //开中断
+	
 }
 
 void main()
 {
-	int i=0;
 	Init();//初始化
 	while(1)
 	{
@@ -79,4 +92,32 @@ void Timer() interrupt 1 //中断程序pwm信号输出
 	if(count == 20)count = 0;
 	if(count < angle/6)pwm = 1;
 	else pwm = 0;
+}
+
+void ser_int (void) interrupt 4
+{
+	if(RI == 1) 
+	{
+		ES=0;		//暂时关闭串口中断
+		//如果收到.
+		RI = 0;      //清除标志.
+		New_rec = 1;
+		UART_buff = SBUF;  //把收到的信息从SBUF放到UART_buff中
+		if(UART_buff == 1 || UART_buff==0x31)  
+		{
+			sw1=0;
+			sw2=1;
+		}
+		else if(UART_buff == 0 || UART_buff==0x30)
+		{
+			sw1=1;
+			sw2=0;
+		}
+	}
+	else
+	{        //如果送毕.
+      TI = 0;      //清除标志.
+      Send_ed = 1;
+    }
+	ES=1;//开启串口中断
 }
