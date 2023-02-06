48fc4b1d55eec1d0150dc5642f91af0e2744018b
iqdxa <1429316040@qq.com>
2023-02-06 20:25:39 +0800

    增加通过Gitee反馈

--- BluetoothSerialPort/app/src/main/java/com/example/bluetoothserialport/AboutActivity.kt
+++ BluetoothSerialPort/app/src/main/java/com/example/bluetoothserialport/AboutActivity.kt
@@ -4,48 +4,37 @@ import android.content.*
 import android.net.Uri
 import android.os.Bundle
 import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import mehdi.sakout.aboutpage.AboutPage
 import mehdi.sakout.aboutpage.Element
+import world.shanya.serialport.SerialPort
+import world.shanya.serialport.SerialPortBuilder
 
 class AboutActivity : AppCompatActivity() {
+
+    private var toolMenu: Menu ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
-        val authorElement = Element()
-        authorElement.gravity = Gravity.CENTER
-        authorElement.title = "作者: TFC"
-        authorElement.setOnClickListener {
-            Toast.makeText(this,"别点啦！都点痛了",Toast.LENGTH_SHORT).show()
+        val feedbackGiteeElement = Element()
+        feedbackGiteeElement.gravity = Gravity.START
+        feedbackGiteeElement.title = "通过Gitee反馈"
+        feedbackGiteeElement.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data=Uri.parse("https://gitee.com/tfc123/SG90-open-the-door/issues")
+            startActivity(intent)
         }
-        val versionElement = Element()
-        val versionName = packageManager.getPackageInfo(packageName,0).versionName
-        versionElement.gravity = Gravity.CENTER
-        versionElement.title = "$versionName"
-        versionElement.setOnClickListener {
-            Toast.makeText(this,"软件版本号：$versionName",Toast.LENGTH_SHORT).show()
-        }
-
-        val updateElement = Element()
-        updateElement.gravity = Gravity.CENTER
-        updateElement.title = "检测更新"
-        updateElement.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            builder.setMessage("暂无更新")
-                .setPositiveButton("确定",
-                    DialogInterface.OnClickListener { _, _ ->
-                    })
-            builder.create()
-        } ?: throw IllegalStateException("Activity cannot be null")
 
         val feedbackGithubElement = Element()
         feedbackGithubElement.gravity = Gravity.START
-        feedbackGithubElement.title = "在GitHub反馈"
+        feedbackGithubElement.title = "通过GitHub反馈"
         feedbackGithubElement.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data=Uri.parse("https://github.com/iqdxa/SG90-open-the-door/issues")
@@ -65,6 +54,28 @@ class AboutActivity : AppCompatActivity() {
             Toast.makeText(this,"邮箱已经复制到剪切板",Toast.LENGTH_SHORT).show()
         }
 
+        val authorElement = Element()
+        authorElement.gravity = Gravity.CENTER
+        authorElement.title = "作者: TFC"
+        authorElement.setOnClickListener {
+            Toast.makeText(this,"别点啦！都点痛了",Toast.LENGTH_SHORT).show()
+        }
+
+        val versionElement = Element()
+        val versionName = packageManager.getPackageInfo(packageName,0).versionName
+        versionElement.gravity = Gravity.CENTER
+        versionElement.title = "$versionName"
+        versionElement.setOnClickListener {
+            Toast.makeText(this,"软件版本号：$versionName",Toast.LENGTH_SHORT).show()
+        }
+
+        val updateElement = Element()
+        updateElement.gravity = Gravity.CENTER
+        updateElement.title = "检测更新"
+        updateElement.setOnClickListener {
+            Toast.makeText(this,"检测更新中……",Toast.LENGTH_SHORT).show()
+        }
+
         val aboutPage: View = AboutPage(this)
             .isRTL(false)
             .setDescription("蓝牙开门手机端")
@@ -77,6 +88,7 @@ class AboutActivity : AppCompatActivity() {
             .addWebsite("https://iqdxa.github.io/SG90-open-the-door/")
             .addGitHub("iqdxa\\SG90-open-the-door")
             .addGroup("反馈：")
+            .addItem(feedbackGiteeElement)
             .addItem(feedbackGithubElement)
             .addItem(feedbackEmailElement)
             .addItem(authorElement)
@@ -99,7 +111,7 @@ class AboutActivity : AppCompatActivity() {
             R.id.menu_main ->
                 startActivity(Intent(this, MainActivity::class.java))
             R.id.menu_quiet -> {
-                serialPort?.disconnect()
+//                serialPort?.disconnect()
                 finish()
             }
         }
